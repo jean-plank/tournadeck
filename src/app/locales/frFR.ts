8abef15ff43ef7685fffdda99f6eb3a771f0d484
Gurimarukin <naxel@laposte.net>
2023-11-18 18:57:43 +0100

    add simple translation

---
+++ src/app/locales/frFR.ts
@@ -0,0 +1,7 @@
+import { Translation } from '@/app/models/Translation'
+
+const frFRTranslation: Translation = {
+  connectWithDiscord: 'Connexion avec Discord',
+}
+
+export default frFRTranslation
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,26 @@
 import Link from 'next/link'
 
+import t from '@/app/locales/frFR'
+
 const Home: React.FC = () => {
   return (
     <div>
       <h1>Home</h1>
-      <nav>
-        <Link href="/toto">/toto</Link>
-        <Link href="/titi">/titi</Link>
-        <a href="#" target="_blank">
-          OUI
-        </a>
+
+      <nav className="flex flex-col">
+        <Link href="/toto" className="underline">
+          /toto
+        </Link>
+        <Link href="/titi" className="underline">
+          /titi
+        </Link>
       </nav>
+
+      <a href="#" className="flex items-center rounded-md bg-discord-blurple px-6 text-white">
+        {t.connectWithDiscord}
+      </a>
+
+      {/* <pre>{JSON.stringify(clientConfig)}</pre> */}
     </div>
   )
 }
