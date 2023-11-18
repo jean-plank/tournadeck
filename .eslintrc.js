27f44e5011a6d748197972102c4e317f546ec401
Gurimarukin <naxel@laposte.net>
2023-11-18 18:58:48 +0100

    update eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   plugins: ['fp-ts', 'prettier', 'tailwindcss'],
   extends: ['next/core-web-vitals', 'plugin:fp-ts/all', 'plugin:tailwindcss/recommended'],
   rules: {
+    'fp-ts/no-module-imports': ['warn', { allowTypes: true }],
     'prettier/prettier': 'error',
     'react/hook-use-state': 'warn',
     'react/jsx-boolean-value': ['warn', 'always'],
