d5faabc62f2609d36039c34e87abdb6bd76fe59f
Gurimarukin <naxel@laposte.net>
2023-11-18 18:58:25 +0100

    update tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,6 @@
 import type { Config } from 'tailwindcss'
+import colors from 'tailwindcss/colors'
+import plugin from 'tailwindcss/plugin'
 
 const config: Config = {
   content: [
@@ -7,9 +9,46 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    colors: {
+      'discord-blurple': '#5865f2',
+      white: colors.white,
+    },
+    extend: {
+      borderRadius: {
+        '1/2': '50%',
+      },
+      boxShadow: {
+        even: '0 0 8px 0 var(--tw-shadow-color)',
+      },
+      gridArea: {
+        1: '1 / 1', // useful for superposing multiple elements in a grid element
+      },
+      textShadow: {
+        DEFAULT: '0 0 5px var(--tw-shadow-color)',
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    /**
+     * Plugin for grid-area:
+     * - `area-{name}`: define grid-area (customize in theme)
+     */
+    plugin(({ matchUtilities, theme }) =>
+      matchUtilities({ area: gridArea => ({ gridArea }) }, { values: theme('gridArea') }),
+    ),
+
+    /**
+     * Plugin for text-shadow:
+     * - `text-shadow-{name}`: add some text-shadow (customize in theme)
+     * - `shadow-{color}`: text-shadow color
+     */
+    plugin(({ matchUtilities, theme }) =>
+      matchUtilities(
+        { 'text-shadow': textShadow => ({ textShadow }) },
+        { values: theme('textShadow') },
+      ),
+    ),
+  ],
 }
 
 export default config
