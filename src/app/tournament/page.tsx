111691c4494654909a7c2fe2d348b9e4553c3a18
Thomas BOCQUET <thomasbocquet.tb@gmail.com>
2023-12-15 20:31:01 +0100

    Formulaire inscription tournois ok

--- src/app/tournament/page.tsx
+++ src/app/tournament/page.tsx
@@ -1,9 +1,37 @@
-import Form from '../../components/TournamentSuscriptionForm'
+'use client'
 
-const TournamentSuscription: React.FC = () => (
-  <div>
-    <Form tournamentId={'tpk4n7503f02ijk'} />
-  </div>
-)
+import PocketBase from 'pocketbase'
+import { useEffect, useState } from 'react'
+
+import Form from '../../components/TournamentSuscriptionForm'
+import { TournamentTile } from '../../components/TournamentTile'
+import { Tournament } from '../../models/Tournament'
+
+const TournamentSuscription: React.FC = () => {
+  const pb = new PocketBase('http://127.0.0.1:8090')
+  const [tournaments, setTournaments] = useState<Tournament[]>([])
+
+  // useEffect(() => {
+  //   pb.collection('tournaments')
+  //     .getFullList<Tournament>({
+  //       sort: '-created',
+  //     })
+  //     .then(res => {
+  //       console.log(res)
+  //       setTournaments(res)
+  //     })
+  // }, [])
+
+  return (
+    <div>
+      <h2>Tournois</h2>
+      {tournaments.map(t => (
+        <TournamentTile data={t} key={t.name} />
+      ))}
+      <h2>Form :</h2>
+      <Form tournamentId={'tpk4n7503f02ijk'} />
+    </div>
+  )
+}
 
 export default TournamentSuscription
---
+++ src/models/Tournament.ts
@@ -0,0 +1,7 @@
+export type Tournament = {
+  id: string
+  name: string
+  start: string
+  end: string
+  maxTeams: number
+}
