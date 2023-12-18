99df33e9763e950af7d1d18cd4e77493f33b0002
Gurimarukin <naxel@laposte.net>
2023-12-18 12:19:46 +0100

    improve redirect signature

--- src/declaration.d.ts
+++ src/declaration.d.ts
@@ -3,3 +3,17 @@ declare type NonEmptyArray<A> = readonly [A, ...A[]]
 declare type ReadonlyRecord<K extends PropertyKey, A> = Readonly<Record<K, A>>
 
 declare type SingleItemArray<A> = readonly [A] | readonly []
+
+// overrides
+
+declare module 'next/navigation' {
+  import type { LinkProps } from 'next/link'
+  import type { RedirectType } from 'next/navigation'
+
+  function redirectFn<RouteInferType>(
+    url: LinkProps<RouteInferType>['href'],
+    type?: RedirectType,
+  ): never
+
+  export { redirectFn as redirect }
+}
