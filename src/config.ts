9d9c0850e013fff8178390318fd4ce875f04e798
Gurimarukin <naxel@laposte.net>
2023-12-19 12:02:28 +0100

    add config

---
+++ src/config.ts
@@ -0,0 +1,23 @@
+import 'server-cli-only'
+
+import { either } from 'fp-ts'
+import { pipe } from 'fp-ts/function'
+import * as D from 'io-ts/Decoder'
+
+import { eitherGetOrThrow } from './utils/fpTsUtils'
+import { decodeError } from './utils/ioTsUtils'
+
+export type Config = D.TypeOf<typeof decoder>
+
+const decoder = D.struct({
+  POCKET_BASE_ADMIN_EMAIL: D.string,
+  POCKET_BASE_ADMIN_PASSWORD: D.string,
+
+  NEXT_PUBLIC_POCKET_BASE_URL: D.string,
+})
+
+export const config: Config = pipe(
+  decoder.decode(process.env),
+  either.mapLeft(decodeError('Config')(process.env)),
+  eitherGetOrThrow,
+)
