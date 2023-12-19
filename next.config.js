0200e808ec901449e34563481379066fc7ea7b6a
Gurimarukin <naxel@laposte.net>
2023-12-19 13:39:49 +0100

    creating pb admin and setting up discord

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    typedRoutes: true,
+    serverComponentsExternalPackages: ['pino'],
+  },
+}
 
 module.exports = nextConfig
