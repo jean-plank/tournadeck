4bcbc88a104085320b38f965dca08f9e73162a7a
Gurimarukin <naxel@laposte.net>
2023-12-24 18:46:59 +0100

    add getSummoner

--- src/services/TheQuestService.ts
+++ src/services/TheQuestService.ts
@@ -1,16 +1,43 @@
 import type { Config } from '../Config'
-import { type HttpClient } from '../helpers/HttpClient'
+import { type HttpClient, statusesToUndefined } from '../helpers/HttpClient'
 import type { GameId } from '../models/riot/GameId'
+import type { Puuid } from '../models/riot/Puuid'
+import type { RiotId } from '../models/riot/RiotId'
 import type { Platform } from '../models/theQuest/Platform'
+import { SummonerShort } from '../models/theQuest/SummonerShort'
 import { TheQuestMatch } from '../models/theQuest/TheQuestMatch'
 
 type TheQuestService = ReturnType<typeof TheQuestService>
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 function TheQuestService(config: Config, httpClient: HttpClient) {
-  return { getMatchById }
+  return { getSummonerByPuuid, getSummonerByRiotId, getMatchById }
 
-  async function getMatchById(platform: Platform, gameId: GameId): Promise<TheQuestMatch> {
+  function getSummonerByPuuid(
+    platform: Platform,
+    puuid: Puuid,
+  ): Promise<SummonerShort | undefined> {
+    return httpClient
+      .get(`${config.THE_QUEST_API_URL}/summoner/byPuuid/${platform}/${puuid}`, {}, [
+        SummonerShort.codec,
+        'SummonerShort',
+      ])
+      .catch(statusesToUndefined(404))
+  }
+
+  function getSummonerByRiotId(
+    platform: Platform,
+    { gameName, tagLine }: RiotId,
+  ): Promise<SummonerShort | undefined> {
+    return httpClient
+      .get(`${config.THE_QUEST_API_URL}/summoner/byRiotId/${platform}/${gameName}/${tagLine}`, {}, [
+        SummonerShort.codec,
+        'SummonerShort',
+      ])
+      .catch(statusesToUndefined(404))
+  }
+
+  function getMatchById(platform: Platform, gameId: GameId): Promise<TheQuestMatch> {
     return httpClient.get(
       `${config.THE_QUEST_API_URL}/madosayentisuto/match/${platform}/${gameId}`,
       { headers: { Authorization: config.THE_QUEST_TOKEN }, cache: 'no-store' },
