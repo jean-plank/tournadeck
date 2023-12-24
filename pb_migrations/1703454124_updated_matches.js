d4780073f43b7b4db914bbed334ffbce89c2df4c
Gurimarukin <naxel@laposte.net>
2023-12-24 23:49:19 +0100

    format

--- pb_migrations/1703454124_updated_matches.js
+++ pb_migrations/1703454124_updated_matches.js
@@ -1,33 +1,38 @@
 /// <reference path="../pb_data/types.d.ts" />
-migrate((db) => {
-  const dao = new Dao(db)
-  const collection = dao.findCollectionByNameOrId("3e0p8fs3vhixq4h")
+migrate(
+  db => {
+    const dao = new Dao(db)
+    const collection = dao.findCollectionByNameOrId('3e0p8fs3vhixq4h')
 
-  // add
-  collection.schema.addField(new SchemaField({
-    "system": false,
-    "id": "qykeqguy",
-    "name": "tournament",
-    "type": "relation",
-    "required": true,
-    "presentable": false,
-    "unique": false,
-    "options": {
-      "collectionId": "3pbep6z2nrnxa9k",
-      "cascadeDelete": false,
-      "minSelect": null,
-      "maxSelect": 1,
-      "displayFields": null
-    }
-  }))
+    // add
+    collection.schema.addField(
+      new SchemaField({
+        system: false,
+        id: 'qykeqguy',
+        name: 'tournament',
+        type: 'relation',
+        required: true,
+        presentable: false,
+        unique: false,
+        options: {
+          collectionId: '3pbep6z2nrnxa9k',
+          cascadeDelete: false,
+          minSelect: null,
+          maxSelect: 1,
+          displayFields: null,
+        },
+      }),
+    )
 
-  return dao.saveCollection(collection)
-}, (db) => {
-  const dao = new Dao(db)
-  const collection = dao.findCollectionByNameOrId("3e0p8fs3vhixq4h")
+    return dao.saveCollection(collection)
+  },
+  db => {
+    const dao = new Dao(db)
+    const collection = dao.findCollectionByNameOrId('3e0p8fs3vhixq4h')
 
-  // remove
-  collection.schema.removeField("qykeqguy")
+    // remove
+    collection.schema.removeField('qykeqguy')
 
-  return dao.saveCollection(collection)
-})
+    return dao.saveCollection(collection)
+  },
+)
