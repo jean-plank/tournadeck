ae7a4274cef02c97ea6e41fefd57ee448c4c9d5b
Gurimarukin <naxel@laposte.net>
2023-12-24 20:32:37 +0100

    [feat/remove-test] remove test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,44 +1,10 @@
-import { revalidateTag } from 'next/cache'
-
-import { createTest, listTest } from '../actions/test'
-import { ClientButton } from '../components/ClientButton'
 import { HomeClient } from '../domain/HomeClient'
 import { UserId } from '../models/pocketBase/tables/User'
 
-const Home: React.FC = async () => {
-  const tests = await listTest()
-
-  return (
-    <div className="flex flex-col gap-4 p-4">
-      <div>
-        <b>tests:</b>
-        <ul>
-          {tests.map(test => (
-            <li key={test.id} className="list-item">
-              {test.label}
-            </li>
-          ))}
-        </ul>
-        <br />
-
-        <ClientButton type="button" onClick={addTest} className="border border-black">
-          Add test
-        </ClientButton>
-      </div>
-
-      <hr />
-
-      <HomeClient key={UserId('')} />
-    </div>
-  )
-}
+const Home: React.FC = async () => (
+  <div className="flex flex-col gap-4 p-4">
+    <HomeClient key={UserId('')} />
+  </div>
+)
 
 export default Home
-
-async function addTest(): Promise<void> {
-  'use server'
-
-  await createTest({ label: new Date().toISOString() })
-
-  revalidateTag(listTest.tag)
-}
--- src/models/pocketBase/Tables.ts
+++ src/models/pocketBase/Tables.ts
@@ -1,6 +1,5 @@
 import type { PbAttendee } from './tables/Attendee'
 import type { PbTeam } from './tables/Team'
-import type { PbTest } from './tables/Test'
 import type { PbTournament } from './tables/Tournament'
 import type { PbUser } from './tables/User'
 import type { PbMatch } from './tables/match/Match'
@@ -11,7 +10,6 @@ export type Tables = {
   attendees: PbAttendee
   matches: PbMatch
   teams: PbTeam
-  test: PbTest
   tournaments: PbTournament
   users: PbUser
 }
