65db8aec0ebc4136443f0477a10ce6234ec85e3c
Gurimarukin <naxel@laposte.net>
2023-12-25 17:11:16 +0100

    format

--- pb_migrations/1703520386_updated_users.js
+++ pb_migrations/1703520386_updated_users.js
@@ -1,16 +1,19 @@
 /// <reference path="../pb_data/types.d.ts" />
-migrate((db) => {
-  const dao = new Dao(db)
-  const collection = dao.findCollectionByNameOrId("_pb_users_auth_")
+migrate(
+  db => {
+    const dao = new Dao(db)
+    const collection = dao.findCollectionByNameOrId('_pb_users_auth_')
 
-  collection.updateRule = null
+    collection.updateRule = null
 
-  return dao.saveCollection(collection)
-}, (db) => {
-  const dao = new Dao(db)
-  const collection = dao.findCollectionByNameOrId("_pb_users_auth_")
+    return dao.saveCollection(collection)
+  },
+  db => {
+    const dao = new Dao(db)
+    const collection = dao.findCollectionByNameOrId('_pb_users_auth_')
 
-  collection.updateRule = "id = @request.auth.id"
+    collection.updateRule = 'id = @request.auth.id'
 
-  return dao.saveCollection(collection)
-})
+    return dao.saveCollection(collection)
+  },
+)
