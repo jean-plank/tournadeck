acf08eed9dc7e3527ff67ead12dda2fbd6186f46
Thomas BOCQUET <thomasbocquet.tb@gmail.com>
2023-12-28 12:38:43 +0100

    Yarn format

--- pb_migrations/1703591538_updated_attendees.js
+++ pb_migrations/1703591538_updated_attendees.js
@@ -1,50 +1,57 @@
 /// <reference path="../pb_data/types.d.ts" />
-migrate((db) => {
-  const dao = new Dao(db)
-  const collection = dao.findCollectionByNameOrId("1sm1qeyu7mzdkdc")
+migrate(
+  db => {
+    const dao = new Dao(db)
+    const collection = dao.findCollectionByNameOrId('1sm1qeyu7mzdkdc')
 
-  // remove
-  collection.schema.removeField("mulebzrn")
+    // remove
+    collection.schema.removeField('mulebzrn')
 
-  // add
-  collection.schema.addField(new SchemaField({
-    "system": false,
-    "id": "bppc9q3a",
-    "name": "seed",
-    "type": "number",
-    "required": false,
-    "presentable": false,
-    "unique": false,
-    "options": {
-      "min": null,
-      "max": null,
-      "noDecimal": false
-    }
-  }))
+    // add
+    collection.schema.addField(
+      new SchemaField({
+        system: false,
+        id: 'bppc9q3a',
+        name: 'seed',
+        type: 'number',
+        required: false,
+        presentable: false,
+        unique: false,
+        options: {
+          min: null,
+          max: null,
+          noDecimal: false,
+        },
+      }),
+    )
 
-  return dao.saveCollection(collection)
-}, (db) => {
-  const dao = new Dao(db)
-  const collection = dao.findCollectionByNameOrId("1sm1qeyu7mzdkdc")
+    return dao.saveCollection(collection)
+  },
+  db => {
+    const dao = new Dao(db)
+    const collection = dao.findCollectionByNameOrId('1sm1qeyu7mzdkdc')
 
-  // add
-  collection.schema.addField(new SchemaField({
-    "system": false,
-    "id": "mulebzrn",
-    "name": "seed",
-    "type": "text",
-    "required": false,
-    "presentable": false,
-    "unique": false,
-    "options": {
-      "min": null,
-      "max": null,
-      "pattern": ""
-    }
-  }))
+    // add
+    collection.schema.addField(
+      new SchemaField({
+        system: false,
+        id: 'mulebzrn',
+        name: 'seed',
+        type: 'text',
+        required: false,
+        presentable: false,
+        unique: false,
+        options: {
+          min: null,
+          max: null,
+          pattern: '',
+        },
+      }),
+    )
 
-  // remove
-  collection.schema.removeField("bppc9q3a")
+    // remove
+    collection.schema.removeField('bppc9q3a')
 
-  return dao.saveCollection(collection)
-})
+    return dao.saveCollection(collection)
+  },
+)
--- pb_migrations/1703618481_updated_tournaments.js
+++ pb_migrations/1703618481_updated_tournaments.js
@@ -1,16 +1,19 @@
 /// <reference path="../pb_data/types.d.ts" />
-migrate((db) => {
-  const dao = new Dao(db)
-  const collection = dao.findCollectionByNameOrId("3pbep6z2nrnxa9k")
+migrate(
+  db => {
+    const dao = new Dao(db)
+    const collection = dao.findCollectionByNameOrId('3pbep6z2nrnxa9k')
 
-  collection.viewRule = ""
+    collection.viewRule = ''
 
-  return dao.saveCollection(collection)
-}, (db) => {
-  const dao = new Dao(db)
-  const collection = dao.findCollectionByNameOrId("3pbep6z2nrnxa9k")
+    return dao.saveCollection(collection)
+  },
+  db => {
+    const dao = new Dao(db)
+    const collection = dao.findCollectionByNameOrId('3pbep6z2nrnxa9k')
 
-  collection.viewRule = null
+    collection.viewRule = null
 
-  return dao.saveCollection(collection)
-})
+    return dao.saveCollection(collection)
+  },
+)
