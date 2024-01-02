d69f8caf162a0982fa79a481cbbe441423ca18c8
Gurimarukin <naxel@laposte.net>
2024-01-02 09:53:05 +0100

    improve server error message

--- src/app/(tournaments)/tournoi/[tournament]/participants/AttendeeForm.tsx
+++ src/app/(tournaments)/tournoi/[tournament]/participants/AttendeeForm.tsx
@@ -100,7 +100,7 @@ export const AttendeeForm: React.FC<Props> = ({ tournament, avalaibleTeamRole, o
 
       createAttendee(tournament, formData)
         .then(onSubscribeOk)
-        .catch(() => setSubmitError('Une erreur inconnue est survenue'))
+        .catch(() => setSubmitError('Erreur serveur — le nom d’invocateur est-il correct ?'))
     }
   }, [inputs, onSubscribeOk, tournament])
 
