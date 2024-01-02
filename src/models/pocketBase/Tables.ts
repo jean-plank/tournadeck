4615c293a0dd0f5ae8ad3cf9b16a09eafce2ec52
Gurimarukin <naxel@laposte.net>
2024-01-02 22:37:47 +0100

    Revert "add teamMembers"
    This reverts commit 35ac6047be95e2869005d39145aea4c727dd88d6.

--- src/models/pocketBase/Tables.ts
+++ src/models/pocketBase/Tables.ts
@@ -1,6 +1,5 @@
 import type { PbAttendee } from './tables/Attendee'
 import type { PbTeam } from './tables/Team'
-import type { PbTeamMember } from './tables/TeamMember'
 import type { PbTournament } from './tables/Tournament'
 import type { PbUser } from './tables/User'
 import type { PbMatch } from './tables/match/Match'
@@ -10,7 +9,6 @@ export type TableName = keyof Tables
 export type Tables = {
   attendees: PbAttendee
   matches: PbMatch
-  teamMembers: PbTeamMember
   teams: PbTeam
   tournaments: PbTournament
   users: PbUser
