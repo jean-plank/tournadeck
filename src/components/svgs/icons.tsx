1e2aea7262a634617e5da8d7ba9038e6eba4b9cf
Gurimarukin <naxel@laposte.net>
2024-11-11 00:03:25 +0100

    fill current color

--- src/components/svgs/icons.tsx
+++ src/components/svgs/icons.tsx
@@ -57,7 +57,10 @@ export const DiceFilled: SVGIcon = ({ className }) => (
 // https://unpkg.com/ionicons@7.1.0/dist/svg/ellipse.svg
 export const EllipseFilled: SVGIcon = props => (
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" {...props}>
-    <path d="M256 464c-114.69 0-208-93.31-208-208S141.31 48 256 48s208 93.31 208 208-93.31 208-208 208z" />
+    <path
+      d="M256 464c-114.69 0-208-93.31-208-208S141.31 48 256 48s208 93.31 208 208-93.31 208-208 208z"
+      fill="currentColor"
+    />
   </svg>
 )
 
