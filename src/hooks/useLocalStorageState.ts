31a79fc9962e9de48c1fbe9539d06e3561df5cf2
Gurimarukin <naxel@laposte.net>
2024-11-15 20:52:35 +0100

    make useLocalStorageState work with SSR

--- src/hooks/useLocalStorageState.ts
+++ src/hooks/useLocalStorageState.ts
@@ -2,50 +2,59 @@ import { either, json } from 'fp-ts'
 import type { LazyArg } from 'fp-ts/function'
 import { pipe } from 'fp-ts/function'
 import type { Codec } from 'io-ts/Codec'
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 
 import { decodeError } from '../utils/ioTsUtils'
 
 /**
  * @param initialState if not found in localStorage
  */
-export const useLocalStorageState = <O, A>(
+export function useLocalStorageState<O, A>(
   key: string,
   [codec, codecName]: Tuple<Codec<unknown, O, A>, string>,
   initialState: A | LazyArg<A>,
-): Tuple<A, React.Dispatch<React.SetStateAction<A>>> => {
-  const [a, setA] = useState<A>(() => {
+): Tuple<A, React.Dispatch<React.SetStateAction<A>>> {
+  const [a, setA] = useState<A>(initialState)
+
+  useEffect(() => {
     const item = localStorage.getItem(key)
 
-    if (item === null) return init(initialState)
+    if (item === null) return
 
-    return pipe(
+    pipe(
       json.parse(item),
       either.mapLeft(either.toError),
       either.chain(u => pipe(codec.decode(u), either.mapLeft(decodeError(codecName)(u)))),
-      either.getOrElse(e => {
+      either.fold(e => {
         console.warn('useLocalStorageState: error while reading from localStorage', e)
-        return init(initialState)
-      }),
+      }, setA),
     )
-  })
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
-  useEffect(
-    () =>
-      pipe(
-        codec.encode(a),
-        json.stringify,
-        either.mapLeft(either.toError),
-        either.fold(
-          e => console.warn('useLocalStorageState: error while writing to localStorage', e),
-          str => localStorage.setItem(key, str),
-        ),
-      ),
-    [a, codec, key],
+  const setAWithLocalStorage: React.Dispatch<React.SetStateAction<A>> = useCallback(
+    action =>
+      setA(prev => {
+        const res = isFunction(action) ? action(prev) : action
+
+        pipe(
+          codec.encode(res),
+          json.stringify,
+          either.mapLeft(either.toError),
+          either.fold(
+            e => console.warn('useLocalStorageState: error while writing to localStorage', e),
+            str => localStorage.setItem(key, str),
+          ),
+        )
+
+        return res
+      }),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [],
   )
 
-  return [a, setA]
+  return [a, setAWithLocalStorage]
 }
 
-const init = <A>(a: A | LazyArg<A>): A => (isFunction(a) ? a() : a)
-const isFunction = <A>(a: A | LazyArg<A>): a is LazyArg<A> => typeof a === 'function'
+const isFunction = <A>(a: React.SetStateAction<A>): a is (prevState: A) => A =>
+  typeof a === 'function'
